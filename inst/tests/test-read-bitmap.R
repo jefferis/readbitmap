314f9bf0810e32fee72ccda1281324e6731c5095
Gregory Jefferis <jefferis@gmail.com>
2011-12-12 11:48:20 +0000

    Check error when reading unsupported image format +test doc improvements

--- inst/tests/test-read-bitmap.R
+++ inst/tests/test-read-bitmap.R
@@ -1,4 +1,4 @@
-context("Reading images with read.image")
+context("Reading images with read.bitmap")
 
 require(pixmap)
 require(jpeg)
@@ -6,13 +6,13 @@ require(jpeg)
 test_that("read.bitmap can load real bmp, jpg, png files", {
       imgfile="../images/real.bmp"
       expect_that(read.bitmap(imgfile),
-          equals(read.bmp(imgfile)))
+          equals(read.bmp(imgfile)),info='bmp problem')
       imgfile="../images/real.jpg"
       expect_that(read.bitmap(imgfile),
-          equals(readJPEG(imgfile)))
+          equals(readJPEG(imgfile)),info='jpg problem')
       imgfile="../images/real.png"
       expect_that(read.bitmap(imgfile),
-          equals(readPNG(imgfile)))
+          equals(readPNG(imgfile)),info='png problem')
     })
 
 test_that("read.bitmap can load a bmp pretending to be a jpg", {
@@ -25,6 +25,12 @@ test_that("read.bitmap can load a bmp pretending to be a jpg", {
 test_that("read.bitmap errors out loading fake bmp when identifying by extension", {
       jpgfile="../images/bmp-pretending-to-be.jpg"
       expect_that(read.bitmap(jpgfile, IdentifyByExtension = TRUE),throws_error())
+    })
+
+test_that("read.bitmap errors out loading a pnm file (not implemented)", {
+      imgfile="../images/real.pnm"
+      expect_that(read.bitmap(imgfile),throws_error())
+      expect_that(read.bitmap(imgfile, IdentifyByExtension = TRUE),throws_error())
     })
 
 test_that("read.bitmap can load a real jpeg identified by extension", {
