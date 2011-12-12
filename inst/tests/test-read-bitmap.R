f02a0879c29d269e713ac776b13f54c92268abaa
Gregory Jefferis <jefferis@gmail.com>
2011-12-12 06:13:26 +0000

    Add tests for regular bmp,jpg,png images

--- inst/tests/test-read-bitmap.R
+++ inst/tests/test-read-bitmap.R
@@ -3,6 +3,18 @@ context("Reading images with read.image")
 require(pixmap)
 require(jpeg)
 
+test_that("read.bitmap can load real bmp, jpg, png files", {
+      imgfile="../images/real.bmp"
+      expect_that(read.bitmap(imgfile),
+          equals(read.bmp(imgfile)))
+      imgfile="../images/real.jpg"
+      expect_that(read.bitmap(imgfile),
+          equals(readJPEG(imgfile)))
+      imgfile="../images/real.png"
+      expect_that(read.bitmap(imgfile),
+          equals(readPNG(imgfile)))
+    })
+
 test_that("read.bitmap can load a bmp pretending to be a jpg", {
       jpgfile="../images/bmp-pretending-to-be.jpg"
       bmp=read.bitmap(jpgfile)
@@ -15,19 +27,19 @@ test_that("read.bitmap errors out loading fake bmp when identifying by extension
       expect_that(read.bitmap(jpgfile, IdentifyByExtension = TRUE),throws_error())
     })
 
-test_that("read.bitmap can load a read jpeg identified by extension", {
+test_that("read.bitmap can load a real jpeg identified by extension", {
       jpgfile="../images/real.jpg"
       expect_that(read.bitmap(jpgfile, IdentifyByExtension = TRUE),
           equals(readJPEG(jpgfile)))
     })
 
-test_that("read.bitmap can load a read png identified by extension", {
+test_that("read.bitmap can load a real png identified by extension", {
       pngfile="../images/real.png"
       expect_that(read.bitmap(pngfile, IdentifyByExtension = TRUE),
           equals(readPNG(pngfile)))
     })
 
-test_that("read.bitmap can load a read bmp identified by extension", {
+test_that("read.bitmap can load a real bmp identified by extension", {
       imgfile="../images/real.bmp"
       expect_that(read.bitmap(imgfile, IdentifyByExtension = TRUE),
           equals(read.bmp(imgfile)))
