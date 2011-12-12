97c41b35de9e4105c0d11fcdf33691ff27d0adb1
Gregory Jefferis <jefferis@gmail.com>
2011-12-12 06:04:51 +0000

    Add tests for reading images when identifying only my file extension

--- inst/tests/test-read-bitmap.R
+++ inst/tests/test-read-bitmap.R
@@ -9,3 +9,26 @@ test_that("read.bitmap can load a bmp pretending to be a jpg", {
       bmp2=read.bmp(jpgfile)
       expect_that(bmp,equals(bmp2))
     })
+
+test_that("read.bitmap errors out loading fake bmp when identifying by extension", {
+      jpgfile="../images/bmp-pretending-to-be.jpg"
+      expect_that(read.bitmap(jpgfile, IdentifyByExtension = TRUE),throws_error())
+    })
+
+test_that("read.bitmap can load a read jpeg identified by extension", {
+      jpgfile="../images/real.jpg"
+      expect_that(read.bitmap(jpgfile, IdentifyByExtension = TRUE),
+          equals(readJPEG(jpgfile)))
+    })
+
+test_that("read.bitmap can load a read png identified by extension", {
+      pngfile="../images/real.png"
+      expect_that(read.bitmap(pngfile, IdentifyByExtension = TRUE),
+          equals(readPNG(pngfile)))
+    })
+
+test_that("read.bitmap can load a read bmp identified by extension", {
+      imgfile="../images/real.bmp"
+      expect_that(read.bitmap(imgfile, IdentifyByExtension = TRUE),
+          equals(read.bmp(imgfile)))
+    })
