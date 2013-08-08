717aba206c867dadc4d42c63f9108e3b2c365271
Gregory Jefferis <jefferis@gmail.com>
2013-08-09 00:35:13 +0100

    whitespace
    reindent after switching to 2-space indentation

--- R/read-any-bitmap.R
+++ R/read-any-bitmap.R
@@ -17,16 +17,16 @@ image_type<-function(source,Verbose=FALSE){
     seek(source, 0)
   magic = readBin(source, what = 0L, n = 8, size = 1L, signed = FALSE)
   if(isTRUE(all.equal(magic[1:2], c(66, 77))))
-		return('bmp')
-	else if(isTRUE(all.equal(magic[1:8], 
-			          c(0x89,0x50,0x4E,0x47,0x0D, 0x0A, 0x1A, 0x0A))) )
-		return('png')
-	else if(isTRUE(all.equal(magic[1:2], c(0xFF, 0xD8))))
-		return('jpg')
-	# otherwise we failed to identify the file
-	if(Verbose) warning("Failed to identify image type of: ",source,
-		        ' with magic: ',format.hexmode(as.raw(magic)))
-	return(NA)
+    return('bmp')
+  else if(isTRUE(all.equal(magic[1:8], 
+          c(0x89,0x50,0x4E,0x47,0x0D, 0x0A, 0x1A, 0x0A))) )
+    return('png')
+  else if(isTRUE(all.equal(magic[1:2], c(0xFF, 0xD8))))
+    return('jpg')
+  # otherwise we failed to identify the file
+  if(Verbose) warning("Failed to identify image type of: ",source,
+        ' with magic: ',format.hexmode(as.raw(magic)))
+  return(NA)
 }
 
 #' Read in a bitmap image in JPEG, PNG or BMP format
@@ -43,22 +43,21 @@ image_type<-function(source,Verbose=FALSE){
 #' @seealso \code{\link[jpeg]{readJPEG},\link[png]{readPNG},\link[bmp]{read.bmp}}
 read.bitmap<-function(f,channel,IdentifyByExtension=FALSE,...){
   
-	if(!file.exists(f)) stop("File: ",f," does not exist.")
-	
+  if(!file.exists(f)) stop("File: ",f," does not exist.")
+  
   if(IdentifyByExtension) 
     ext=tolower(sub(".*\\.([^.]+)$","\\1",f))
   else
     ext=image_type(f)
-  
-	if(ext=='png'){
-		im=readPNG(f,...)
-	} else if (ext == 'jpeg' || ext == 'jpg'){
-		im=readJPEG(f,...)
-	} else if (ext == 'bmp'){
-		im=read.bmp(f,...)
-	} else {
-		stop("File f: ",f," does not appear to be a PNG, BMP or JPEG")
-	}
-	if(!missing(channel) && length(dim(im))==3) im=im[,,channel]
-	im
+  if(ext=='png'){
+    im=readPNG(f,...)
+  } else if (ext == 'jpeg' || ext == 'jpg'){
+    im=readJPEG(f,...)
+  } else if (ext == 'bmp'){
+    im=read.bmp(f,...)
+  } else {
+    stop("File f: ",f," does not appear to be a PNG, BMP or JPEG")
+  }
+  if(!missing(channel) && length(dim(im))==3) im=im[,,channel]
+  im
 }
